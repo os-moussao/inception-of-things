8ac49e75509e8ef752e1b239fc54471f583c8804
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-09-25 12:47:01 +0100

    chore: add setup script for runing services

---
+++ playground/k3s-postgres-adminer-exercise/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+kubectl apply -f /vagrant/postgres/pg-configmap.yaml
+kubectl apply -f /vagrant/postgres/pg-secret.yaml
+kubectl apply -f /vagrant/postgres/pg.yaml
+kubectl apply -f /vagrant/adminer/adminer.yaml
