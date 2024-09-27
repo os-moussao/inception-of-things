7d341a03fecb27ade10d2017f7fa22b4caea124e
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-09-27 16:54:01 +0100

    chore: make adminer-service internal & add adminer-ingress

--- playground/k3s-postgres-adminer-exercise/setup.sh
+++ playground/k3s-postgres-adminer-exercise/setup.sh
@@ -3,3 +3,4 @@ kubectl apply -f /vagrant/postgres/pg-configmap.yaml
 kubectl apply -f /vagrant/postgres/pg-secret.yaml
 kubectl apply -f /vagrant/postgres/pg.yaml
 kubectl apply -f /vagrant/adminer/adminer.yaml
+kubectl apply -f /vagrant/adminer/adminer-ingress.yaml
