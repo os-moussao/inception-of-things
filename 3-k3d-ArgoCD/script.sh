793d2689f0950513f0e244190e9f0caa57187343
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-11-12 09:59:36 +0100

    refcator: move argocd part to home

--- 3-k3d-ArgoCD/script.sh
+++ 3-k3d-ArgoCD/script.sh
@@ -33,7 +33,7 @@ IP=$(hostname -I | awk '{print $1}')
 export ARGOCD_USER=admin
 export ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 > /dev/null 2>&1 &
-echo "Server running on: $(hostname -I | awk '{print $1}'):8080"
+echo "Access server on: https://$(hostname -I | awk '{print $2}'):8080"
 echo "USERNAME: $ARGOCD_USER"
 echo "PASSWORD: $ARGOCD_PASSWORD"
 echo "USERNAME=$ARGOCD_USER" > /home/vagrant/argo-credentials.txt
